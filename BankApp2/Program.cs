96537784a96a57b63e352d749a74f83099a5ba66
Anna Axelsson <a.axelsson51@gmail.com>
2023-01-06 15:10:05 +0100

    added testPincode function to be called from makeWithdrawalMethod

--- BankApp2/Program.cs
+++ BankApp2/Program.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics.Metrics;
 using System.Reflection;
 using System.Threading;
-using Internal;
+//using Internal;
 using static BankApp2.Program;
 
 // Användaren ska mata in sitt användarnummer/användarnamn (valfritt hur detta ser ut)...
@@ -13,7 +13,7 @@ using static BankApp2.Program;
 //När användaren lyckats logga in ska bankomaten fråga vad användaren vill göra. Det ska finnas fyra val:
 //se konton o saldo överföringar mellan konton, ta ut pengar, logga ut
 
-//TODONär en funktion har kört klart ska användaren få upp texten "Tryck enter för att komma till huvudmenyn".
+//När en funktion har kört klart ska användaren få upp texten "Tryck enter för att komma till huvudmenyn".
 //När användaren tryckt enter kommer menyn upp igen.
 
 //Om användaren väljer "Logga ut" ska programmet inte stänga av. Användaren ska komma till inloggningen igen.
@@ -26,7 +26,7 @@ using static BankApp2.Program;
 // Saldon för alla konton sätts vid starten av programmet (du ställer in en en summa som
 //finns på varje konto i koden) så om programmet startas om återställs alla saldon.
 
-//TODO när anv väljer överföringar mellan konton ska hen kunna välja ett konto att ta pengar från,
+//när anv väljer överföringar mellan konton ska hen kunna välja ett konto att ta pengar från,
 //ett konto att flytta pengarna till och sen en summa som ska flyttas mellan dessa, efter summan flyttas
 //ska användaren få se vilken summa som finns på dessa två konton som påverkades.
 
@@ -35,7 +35,7 @@ using static BankApp2.Program;
 //TODO när anv väljer ta ut pengar Användaren ska kunna välja ett av sina konton samt en summa
 //Efter detta måste användaren skriva in sin pinkod för att bekräfta att de vill ta ut pengar
 
-//TODO Pengarna ska sedan tas bort från det konto som valdes Sist av allt ska systemet skriva ut det nya
+//Pengarna ska sedan tas bort från det konto som valdes Sist av allt ska systemet skriva ut det nya
 //saldot på det kontot.
 
 namespace BankApp2;
@@ -145,7 +145,7 @@ class Program
                 string? userPinCode = Console.ReadLine();
                 foreach (var user in users)
                 {
-                    if ((user.userName.Equals(userPinCode)) && (user.pinCode.Equals(userPinCode)))
+                    if ((user.userName.Equals(userName)) && (user.pinCode.Equals(userPinCode)))
                     {
                         //Console.WriteLine(user.userName);
                         //Console.WriteLine(user.pinCode);
@@ -355,6 +355,7 @@ class Program
 
         void withdrawal(User user)
         {
+            testPincode(user);
             listAccounts(user);
             int foundWithdrawalAccount = selectWithdrawalAccount(user);
 
@@ -364,6 +365,35 @@ class Program
 
             makeWithdrawal(foundWithdrawalAccount, withdrawalAmount, user);
         }
+
+        void testPincode(User user){
+            int userTries = 0;
+            bool testPincode = true;
+            do
+            {
+                Console.WriteLine("Var god ange din pinkod");
+                string? userPinCode = Console.ReadLine();
+                foreach (var scannedUser in users)
+                {
+                    if (user.pinCode.Equals(userPinCode))
+                    {
+                        Console.WriteLine(user.pinCode);
+                        Console.WriteLine(userPinCode);
+                        testPincode = false;
+                        mainMenu(user);
+                    }
+                }
+                userTries++;
+                if (userTries > 1)
+                {
+                    Console.WriteLine("Du har angett fel pinkod tre gånger, var god vänta 3 minuter innan du försöker igen");
+                    Console.WriteLine("Sleep for 3 minutes");
+                    Thread.Sleep(180000);
+                }
+
+            } while (testPincode);
+        } return;
+
 
         void makeWithdrawal(int foundWithdrawalAccount, decimal withdrawalAmount, User user)
         {
@@ -397,116 +427,8 @@ class Program
 
 
 
-    /*//TRANSFER FUNDS BETWEEN ACCOUNTS
-    void transferFunds()
-    {
-        //lista personens konton
-        foreach (Account in loggedInUser.accounts)
-        {
-            Console.WriteLine(Account.accountValues);
-        }
-        //fråga från vilket konto
-        Console.WriteLine("Please enter what account you would like to transfer the sumb from: ");
-        string? input1 = Console.ReadLine();
-        int withdrawalAccount = Int32.Parse(input);
-        if (withdrawalAccount.amount < 0)
-        {
-            throw new InvalidOperationException("Not sufficient funds for this withdrawal");
-        }
-        //fråga om summa
-        Console.WriteLine("Please enter the amount you would like to transfer: ");
-        string? input = Console.ReadLine();
-        int transferAmount = Int32.Parse(input);
-        if (transferAmount <= 0)
-        {
-            throw new ArgumentOutOfRangeException(nameof(transferAmount), "amount of withdrawan must be positive");
-        }
-        //fråga till vilket konto
-        Console.WriteLine("Please enter what account you would like to transfer the sumb to: ");
-        string? input2 = Console.ReadLine();
-        int destinationAccount = Int32.Parse(input);
-
-
-
-        int newSumbOnDestinationAccount = destinationAccount + withdrawalAccount;
-        Console.WriteLine("You have successfylly transferred " + transferAmount);
-        Console.WriteLine(+" now contains " + );
-        Console.WriteLine(+" now contains " + );
-
-        Console.WriteLine("Tryck enter för att komma till huvudmenyn");
-        //if (e.Key == Key.Return)
-        if (e.KeyValue == 13)
-        {
-            mainMenu();
-        }
+    
     }
-
-    //SKAPAR PLATS FÖR ETT NYTT KONTO I ARRAYEN AV KONTON OCH LÄGGER TILL ETT KONTO??
-    //metod addMonster som får en array av monster (oldMonsters) och ett nytt att lägga till (monsterToadd)
-    //returnerar en ny array av monster med ett extra tillagt
-
-    User[] addUser(User[] oldUsers, User userToAdd)
-    {
-        int a = users.Length;
-        int numberOfUsers = oldUsers.Length;                            //spara längden av gamla kontoarrayen oldMonsters  
-        User[] newUsers = new User[numberOfUsers + 1];    //skapa ny array newMonsters med en til plats
-        for (int l = 0; l < numberOfUsers; l++)
-        {
-            newUsers[l] = oldUsers[l];                          //kopiera värdena till den nya
-        }
-        newUsers[numberOfUsers] = userToAdd;                 //lägger till ett nytt monster
-        return newUsers;
-    }
-    */
-
-
-
-
-        /*
-        //CREATE NEW ACCOUNT MONSTER?
-        // monsters = addMonster(monsters, parseRow("Tobbe, 20,1+,12"));
-        //Console.WriteLine("Monster successfully added to database");
-
-        User parseRow(string monsterRow)       //får in tex stringen "Tobbe, 20,1+,12"
-        {
-            var cols = monsterRow.Split(',');      //splittar den 
-            User m = new User();             //skapar ett nytt monsterobjekt
-
-            for (int k = 0; k < cols.Length; k++)             //lopar igenom antal vrden man skickade
-            {
-                Console.WriteLine("Inner for each loop" + k + "col" + cols[k]);       //skriver ut varje index k och dess värde
-                switch (k)
-                {
-                    case 0:
-                        m.name = cols[k];          //sätter 0 värdet (tobbe) till name
-                        break;
-                    case 1:
-                        m.Health = int.Parse(cols[k]);      //sätter andra värdet (20) till health
-                        break;
-                    case 2:
-                        break;
-                    case 3:
-                        m.Defense = int.Parse(cols[k]);       //sätter sista värdet till defense
-                        break;
-
-
-                }
-
-            }
-            return m;     //returnerar det nya monster objektet*/
-    }
-
-
-
-
-    //updatedUser = Users[0];
-    //Om vi relaterar det till banken kan users [0] va ett antal users som vi stegar igenom
-    //Ungefär som login labben att man scannar alla users och kollar om anv fyllt i rätt
-    //anvnamn och pinkod för en viss user då ör det luoggedin / current user då brukar man
-    //lägga den i en egen variabel eller det är ett sätt iaf (ganska tidigt i 15/12).....
-
-
-
 
 
     public class Account
@@ -544,17 +466,6 @@ class Program
     }
 
     public class User {
-
-        //{ get; set; }
-        //private byte[][] array;
-
-       /* public User(string name, string userName, string pinCode, Account[] accounts)
-        {
-            this.name = name;
-            this.userName = userName;
-            this.pinCode = pinCode;
-            this.accounts = accounts;
-        }*/
 
         public string name
         {
@@ -601,6 +512,6 @@ class Program
             }
         }
     }
-}
+
 
 
